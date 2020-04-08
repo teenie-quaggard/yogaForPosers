443ebb8cef2ba873bc88698965af212048ad7e1e
Kristina Jaggard <kjaggard@8thlight.com>
2020-04-08 13:32:25 +0100

    reset database between tests, build table as part of connection setup

--- src/main/java/space/jaggard/yogaforposers/database/Database.java
+++ src/main/java/space/jaggard/yogaforposers/database/Database.java
@@ -17,55 +17,121 @@ public class Database {
         this.connectionString = connectionString;
     }
 
-    public Connection connectToDB() throws ClassNotFoundException, SQLException {
+    public void DBSetup() throws ClassNotFoundException, SQLException {
         try {
             if (connection == null){
                 Class.forName("org.sqlite.JDBC");
                 connection = DriverManager.getConnection(connectionString);
+                createTable();
             }
         } catch (Exception e) {
             System.out.println("Error: " + e);
             e.printStackTrace();
         }
-        return connection;
     }
 
-//
-//    public void addEntry(Entry entry) throws SQLException, ClassNotFoundException {
-//        connectToDB();
-//        try {
-//            Statement statement = connection.createStatement();
-//
-//            String englishName = entry.getEnglishName();
-//            String sanskritName = entry.getSanskritName();
-//            String poseType = entry.getPoseType();
-//            String healthBenefits = entry.getHealthBenefits();
-//            String imgURL = entry.getImgURL();
-//
-//            int addEntry = statement.executeUpdate(
-//                    "INSERT INTO yogaPoses "
-//                            + "(englishName, sanskritName, poseType, healthBenefits, imgURL) "
-//                            + "VALUES('" + englishName + "', '" + sanskritName + "', '"
-//                            + poseType + "', '" + healthBenefits + "', '" + imgURL + "');");
-//
-//            statement.close();
-//
-////            PreparedStatement statement = connection.prepareStatement("INSERT" +
-////                    " INTO yogaPoses VALUES(?,?,?,?,?,?)");
-////            statement.setString(0, entry.getEnglishName());
-////            statement.setString(1, entry.getSanskritName());
-////            statement.setString(2, entry.getPoseType());
-////            statement.setString(3, entry.getHealthBenefits());
-////            statement.setString(4, entry.getHealthBenefits());
-////            statement.setString(5, entry.getImgURL());
-////            statement.execute();
-////            statement.close();
-//        } catch (Exception e) {
-//            System.out.println("Error: " + e);
-//            e.printStackTrace();
-//        }
-//
-//    }
+    public void createTable(){
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE yogaPoses(id text, " +
+                    "englishName text, sanskritName text, poseType text, " +
+                    "healthBenefits text, imgURL text, primary key(id));");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void addEntry(Entry entry) throws SQLException, ClassNotFoundException {
+        DBSetup();
+        try {
+            Statement statement = connection.createStatement();
+
+            String id = entry.getID();
+            String englishName = entry.getEnglishName();
+            String sanskritName = entry.getSanskritName();
+            String poseType = entry.getPoseType();
+            String healthBenefits = entry.getHealthBenefits();
+            String imgURL = entry.getImgURL();
+
+            int addEntry = statement.executeUpdate(
+                    "INSERT INTO yogaPoses (id, englishName, sanskritName, poseType, healthBenefits, imgURL) "
+                            + "VALUES('" + id + "', '" + englishName + "', '" +
+                            sanskritName + "', '" + poseType + "', '" + healthBenefits + "', '" + imgURL + "');");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Entry> getEntry(String id) throws SQLException,
+            ClassNotFoundException {
+        DBSetup();
+        ArrayList<Entry> entryResults = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT englishName, " +
+                    "sanskritName, poseType, healthBenefits, imgURL FROM yogaPoses WHERE id" +
+                    " ='" + id + "';");
+
+            boolean resultSet = result.next();
+
+            if (resultSet) {
+                String englishName = result.getString("englishName");
+                String sanskritName = result.getString("sanskritName");
+                String poseType = result.getString("poseType");
+                String healthBenefits = result.getString(
+                        "healthBenefits");
+                String imgURL  = result.getString("imgURL");
+
+                statement.close();
+                Entry entry = new Entry(englishName, sanskritName, poseType,
+                        healthBenefits, imgURL);
+                entryResults.add(entry);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            e.printStackTrace();
+        }
+        return entryResults;
+    };
+
+
+    public void clearDB() throws SQLException, ClassNotFoundException {
+        DBSetup();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("DROP TABLE yogaPoses;");
+            statement.close();
+            connection.close();
+            System.out.println("Connection closed.");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public int countDatabaseEntries() throws SQLException, ClassNotFoundException {
+        DBSetup();
+        int numberOfEntries = 0;
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT COUNT(*) FROM" +
+                    " yogaPoses");
+            numberOfEntries = result.getInt(1);
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return numberOfEntries;
+    }
+
+
+}
+
+
 //
 //    public ArrayList<Entry> getEntries() throws SQLException, ClassNotFoundException {
 //        connectToDB();
@@ -97,77 +163,32 @@ public class Database {
 //        return null;
 //    }
 
-    public Entry getEntry(int id) throws SQLException, ClassNotFoundException {
-        connectToDB();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT englishName, " +
-                    "sanskritName, " +
-                    "poseType, healthBenefits, imgURL FROM yogaPoses WHERE id" +
-                    " ='" + id + "';");
 
-            boolean resultSet = result.next();
-
-            if (resultSet) {
-                String englishName = result.getString("englishName");
-                String sanskritName = result.getString("sanskritName");
-                String poseType = result.getString("poseType");
-                String healthBenefits = result.getString(
-                        "healthBenefits");
-                String imgURL  = result.getString("imgURL");
-
-                statement.close();
-                return new Entry(englishName, sanskritName, poseType,
-                        healthBenefits, imgURL);
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-    public void clearDB() throws SQLException, ClassNotFoundException {
-        connectToDB();
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("DELETE FROM yogaPoses;");
-            statement.close();
-            connection.close();
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-            e.printStackTrace();
-        }
-    }
-
-    public void initialiseDummyData() throws SQLException {
-
-
-        Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery("SELECT name FROM " +
-                "sqlite_master WHERE type='table' AND name='yogaPoses'");
-
-
-        if(!result.next()){
-            System.out.println("Building the yogaPoses table with " +
-                    "prepopulated values.");
-            Statement statement2 = connection.createStatement();
-            statement2.execute("CREATE TABLE yogaPoses(id integer, " +
-                    "englishName text, sanskritName text, poseType text, " +
-                    "healthBenefits text, imgURL text, primary key(id));");
-
-            PreparedStatement entry = connection.prepareStatement(
-                    "INSERT INTO yogaPoses values(0, 'Pigeon Pose', 'Eka Pada Rajakapotasana', 'Hip opener', 'Opens hip joint', 'https://cdn.sivanaspirit.com/wp-content/uploads/2017/11/04001203/un-94-896x580.jpg');"
-                );
-            entry.execute();
-
-            PreparedStatement entry2 = connection.prepareStatement("INSERT" +
-                    " INTO yogaPoses values(1, 'Corpse Pose', 'Savasana'," +
-                    " 'Resting pose', 'Relaxation', 'https://www.sarvyoga.com/wp-content/uploads/2016/05/Savasana-Corpse-yoga-Pose-Relaxation-Pose.jpg');");
-            entry2.execute();
-
-            }
-        }
-    }
-
+//    public void initialiseDummyData() throws SQLException {
+//
+//
+//        Statement statement = connection.createStatement();
+//        ResultSet result = statement.executeQuery("SELECT name FROM " +
+//                "sqlite_master WHERE type='table' AND name='yogaPoses'");
+//
+//
+//        if(!result.next()){
+//            System.out.println("Building the yogaPoses table with " +
+//                    "prepopulated values.");
+//            Statement statement2 = connection.createStatement();
+//            statement2.execute("CREATE TABLE yogaPoses(id integer, " +
+//                    "englishName text, sanskritName text, poseType text, " +
+//                    "healthBenefits text, imgURL text, primary key(id));");
+//
+//            PreparedStatement entry = connection.prepareStatement(
+//                    "INSERT INTO yogaPoses values(0, 'Pigeon Pose', 'Eka Pada Rajakapotasana', 'Hip opener', 'Opens hip joint', 'https://cdn.sivanaspirit.com/wp-content/uploads/2017/11/04001203/un-94-896x580.jpg');"
+//                );
+//            entry.execute();
+//
+//            PreparedStatement entry2 = connection.prepareStatement("INSERT" +
+//                    " INTO yogaPoses values(1, 'Corpse Pose', 'Savasana'," +
+//                    " 'Resting pose', 'Relaxation', 'https://www.sarvyoga.com/wp-content/uploads/2016/05/Savasana-Corpse-yoga-Pose-Relaxation-Pose.jpg');");
+//            entry2.execute();
+//
+//            }
+//        }
--- src/test/java/space/jaggard/yogaforposers/database/DatabaseTest.java
+++ src/test/java/space/jaggard/yogaforposers/database/DatabaseTest.java
@@ -8,6 +8,7 @@ import space.jaggard.yogaforposers.entry.Entry;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,33 +19,37 @@ class DatabaseTest {
     @BeforeEach
     private void setUp() throws SQLException, ClassNotFoundException {
         database = new Database(Database.TEST_CONNECTION_STRING);
-        database.connectToDB();
+        System.out.println("Database initialised");
     }
 
     @AfterEach
     private void tearDownDatabase() throws SQLException, ClassNotFoundException {
+        System.out.println("Wiping test database.");
         database.clearDB();
     }
 
-//    @Test
-//    void addEntry() throws SQLException, ClassNotFoundException {
-//        Entry entry = new Entry("Pigeon Pose", "Eka Pada Rajakapotasana", "Hip " +
-//                "opener", "Opens hip joint", "");
-//        database.addEntry(entry);
-//        Entry dbEntry = database.getEntry(0);
-//        String dbEntryName = dbEntry.getEnglishName();
-//
-//        assertEquals("Pigeon Pose" , dbEntryName);
-//    }
+    @Test
+    void addEntry() throws SQLException, ClassNotFoundException {
+        Entry entry = new Entry("Pigeon Pose", "Eka Pada Rajakapotasana", "Hip " +
+                "opener", "Opens hip joint", "");
+
+        database.addEntry(entry);
+
+        assertEquals(1, database.countDatabaseEntries());
+    }
 
     @Test
     void getEntry() throws SQLException, ClassNotFoundException {
-        database.initialiseDummyData();
-        Entry entry = database.getEntry(0);
-        String entryName = entry.getEnglishName();
+        Entry entry = new Entry("Pigeon Pose", "Eka Pada Rajakapotasana", "Hip " +
+                "opener", "Opens hip joint", "");
+        String id = entry.getID();
+
+        database.addEntry(entry);
+
+        ArrayList<Entry> results = database.getEntry(id);
+        String entryName = results.get(0).getEnglishName();
 
         assertEquals("Pigeon Pose", entryName);
-
     }
 //
 //    @Test
